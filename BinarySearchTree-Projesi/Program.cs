54f81b951988f6a7f107087c5228a180a12240c8
Wbck <meertmrv@gmail.com>
2022-02-09 19:06:58 +0300

    First Commit

---
+++ BinarySearchTree-Projesi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BinarySearchTree_Projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[7,5,1,8,3,6,0,9,4,2]->Binary Search Tree
+            /*
+                            7
+                        /        \
+                        4        8
+                    /      \   /    \
+                    1      6   9    2
+                /           \  / 
+                5           0  3
+                    
+            */
+        }
+    }
+}
