8964e0f5758b9549c0814ae49dada0cd62941d7d
Wbck <meertmrv@gmail.com>
2022-02-09 19:08:39 +0300

    First Commit

---
+++ InsertionSort-Projesi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InsertionSort_Projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[22,27,16,2,18,6]-> Insertion Sort
+            //1->Siralama asamalari
+            //    1-22,27,16,2,18,6
+            //    2-16,22,27,2,18,6
+            //    3-2,16,22,27,18,6
+            //    4-2,16,18,22,27,6
+            //    5-2,6,16,18,22,27
+            
+            //2->Big O(n^2)
+
+            //3->Time Complexity
+            //   Best case:16
+            //   Average case:2
+            //   Worst case:6
+
+            //4->Dizi siralandiktan sonra 18 sayisi average case kapsamina girer.
+
+            //5->[7,3,5,8,2,9,4,15,6]
+            //    1-7,3,5,8,2,9,4,15,6
+            //    2-3,7,5,8,2,9,4,15,6
+            //    3-3,5,7,8,2,9,4,15,6
+            //    4-3,5,7,8,2,9,4,15,6
+            //    5-2,3,5,7,8,9,4,15,6
+
+        }
+    }
+}
---
+++ MergeSort-Projesi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MergeSort_Projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            [16,21,11,8,12,22]->Merge Sort
+                    [16,21,11,8,12,22]
+                        /           \
+                    [16,21,11]      [8,12,22]
+                      /    \          /    \
+                    [16,21]  [11]   [8,12] [22]
+                        |                |
+                    [11,16,21]       [8,12,22]
+                        \               /
+                        [8,11,12,16,21,22]
+                    
+            */
+                        //Big O(nlogn)
+        }
+    }
+}
